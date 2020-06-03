9b7747dc98928237e6ec5ea82e9427f52d2b6dc8
masariuman <masariuman@gmail.com>
2020-06-03 15:44:49 +0700

    migration diklat_fungsional

---
+++ database/migrations/2020_06_03_083841_create_table_pendidikan_diklat_fungsional.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablePendidikanDiklatFungsional extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diklat_fungsional', function (Blueprint $table) {
+            $table->id();
+            $table->string('tahun');
+            $table->foreignID('pegawai_id')->constrained('pegawai');
+            $table->string('nama_diklat');
+            $table->enum('tempat_belajar',['1','2']);
+            $table->string('lokasi');
+            $table->datetime('tanggal_mulai');
+            $table->datetime('tanggal_selesai');
+            $table->string('jumlah_jam');
+            $table->string('penyelenggara');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diklat_fungsional');
+    }
+}
