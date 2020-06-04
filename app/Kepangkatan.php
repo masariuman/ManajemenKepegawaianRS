daaadc0153cf9045f4ce61c2d7cc616fd9a24c72
masariuman <masariuman@gmail.com>
2020-06-04 15:17:39 +0700

    kepangkatan model

--- app/Kepangkatan.php
+++ app/Kepangkatan.php
@@ -7,9 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Kepangkatan extends Model
 {
     //
-    protected $table = '';
+    protected $table = 'kepangkatan';
     protected $fillable = [
-
+        'tahun',
+        'pegawai_id',
+        'golongan',
+        'tmt_golongan',
+        'penjabat_penandatangan_sk',
+        'nomor_sk',
+        'tanggal_sk',
         'created_at',
         'updated_at'
     ];
