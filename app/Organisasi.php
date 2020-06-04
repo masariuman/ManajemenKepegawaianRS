e4f21bd1d54577965d17d4e4dd84c2489b5436f6
masariuman <masariuman@gmail.com>
2020-06-04 15:19:11 +0700

    organisasi

--- app/Organisasi.php
+++ app/Organisasi.php
@@ -7,9 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Organisasi extends Model
 {
     //
-    protected $table = '';
+    protected $table = 'organisasi';
     protected $fillable = [
-
+        'tahun',
+        'pegawai_id',
+        'tahun_organisasi',
+        'nama_organisasi',
+        'kedudukan',
+        'tanggal_mulau',
+        'tanggal_selesai',
+        'nomor_sk',
+        'jabatan_pembuat_sk',
         'created_at',
         'updated_at'
     ];
