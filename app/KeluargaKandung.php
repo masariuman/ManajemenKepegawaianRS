01dc931e01ec60cf5f24f659242537679d935b3c
masariuman <masariuman@gmail.com>
2020-06-04 15:15:21 +0700

    keluarga kandung model

--- app/KeluargaKandung.php
+++ app/KeluargaKandung.php
@@ -7,9 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class KeluargaKandung extends Model
 {
     //
-    protected $table = '';
+    protected $table = 'keluarga_kandung';
     protected $fillable = [
-
+        'tahun',
+        'pegawai_id',
+        'nama',
+        'hubungan',
+        'pekerjaan',
+        'tanggal_lahir',
+        'jenis_kelamin',
+        'kondisi',
         'created_at',
         'updated_at'
     ];
