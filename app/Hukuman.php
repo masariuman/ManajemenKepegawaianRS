1276df3049f17c53e3fd9e458ab563858bc9449f
masariuman <masariuman@gmail.com>
2020-06-04 15:05:28 +0700

    hukuman model

--- app/Hukuman.php
+++ app/Hukuman.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Hukuman extends Model
 {
     //
+    protected $table = 'hukuman';
+    protected $fillable = [
+        'tahun',
+        'pegawai_id',
+        'kode_hukuman',
+        'nomor_sk',
+        'tanggal_sk',
+        'tmt_berlaku',
+        'penjabat_pembuat_sk',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function pegawai()
+    {
+        return $this->belongsTo('App\Pegawai', 'pegawai_id',);
 }
