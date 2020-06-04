2571a44a276d6b149be38d2aff518663a40f694b
masariuman <masariuman@gmail.com>
2020-06-04 14:39:01 +0700

    model anak

--- app/Anak.php
+++ app/Anak.php
@@ -7,4 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class Anak extends Model
 {
     //
+    protected $table = 'anak';
+    protected $fillable = [
+    	'tahun',
+        'pegawai_id',
+        'nama',
+        'jenis_kelamin',
+        'tempat_lahir',
+        'tanggal_lahir',
+        'status_anak',
+        'pendidikan',
+        'pekerjaan',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function pegawai()
+    {
+        return $this->hasMany('App\Pegawai', 'pegawai_id',);
+    }
 }
