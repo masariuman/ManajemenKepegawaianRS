4fcd2c7d69105d9df54574932951cdfa72128785
masariuman <masariuman@gmail.com>
2020-06-04 14:51:09 +0700

    anak model

--- app/DiklatFungsional.php
+++ app/DiklatFungsional.php
@@ -7,4 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class DiklatFungsional extends Model
 {
     //
+    protected $table = 'anak';
+    protected $fillable = [
+        'tahun',
+        'pegawai_id',
+        'nama_diklat',
+        'tempat_belajar',
+        'lokasi',
+        'tanggal_mulai',
+        'tanggal_selesai',
+        'jumlah_jam',
+        'penyelenggara',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function pegawai()
+    {
+        return $this->belongsTo('App\Pegawai', 'pegawai_id',);
+    }
 }
