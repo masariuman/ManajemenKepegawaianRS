60424586eb39316ba1f2cc06d440f2e9a3fa703e
masariuman <masariuman@gmail.com>
2020-06-04 15:12:58 +0700

    jabatan teknis model

--- app/JabatanTeknis.php
+++ app/JabatanTeknis.php
@@ -7,9 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class JabatanTeknis extends Model
 {
     //
-    protected $table = '';
+    protected $table = 'jabatan_teknis';
     protected $fillable = [
-
+        'tahun',
+        'pegawai_id',
+        'nama_jabatan',
+        'tmt_jabatan',
+        'tahun_mulai',
+        'tahun_selesai',
+        'nomor_sk',
+        'tanggal_sk',
+        'nip_penjabat_penandatangan_sk',
+        'nip_lama_penjabat_penandatangan_sk',
         'created_at',
         'updated_at'
     ];
