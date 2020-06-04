016996848bbf2295a15964a50329b8a52aa67e53
masariuman <masariuman@gmail.com>
2020-06-04 15:08:17 +0700

    jabatan fungsional model

--- app/JabatanFungsional.php
+++ app/JabatanFungsional.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class JabatanFungsional extends Model
 {
     //
+    protected $table = 'jabatan_fungsional';
+    protected $fillable = [
+        'tahun',
+        'pegawai_id',
+        'eselon',
+        'nama_jabatan',
+        'tmt_jabatan',
+        'nomor_sk',
+        'tanggal_sk',
+        'penjabat_penandatangan_sk',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function pegawai()
+    {
+        return $this->belongsTo('App\Pegawai', 'pegawai_id',);
 }
