2cc328e9e65a79970010c526f1263b2904f2a744
masariuman <masariuman@gmail.com>
2020-06-08 14:45:19 +0700

    adsa

--- app/Http/Controllers/riwayatPendidikanFormalController.php
+++ app/Http/Controllers/riwayatPendidikanFormalController.php
@@ -3,104 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\PendidikanFormal;
 
-class riwayatPendidikanFormalController extends Controller
+class RiwayatPendidikanFormalController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $data_pendidikan_formal = PendidikanFormal::get();
-    
-        return $data_pendidikan_formal;
-      
-        // return view('riwayat_pendidikan_formal');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
- 
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $pendidikan_formal = PendidikanFormal::insert([
-            'tahun' => $request->input('tahun', 2019),
-            'pegawai_id' => $request->input('pegawai_id', 1),
-            'tingkat_pendidikan' => $request->tingkat_pendidikan,
-            'nama_sekolah' => $request->nama_sekolah,
-            'jurusan_prodi' => $request->jurusan_prodi,
-            'tahun_masuk' => $request->tahun_masuk,
-            'tahun_lulus' => $request->tahun_lulus,
-            'tempat_belajar' => $request->tempat_belajar,
-            'lokasi' => $request->lokasi,
-            'nomor_ijazah' => $request->nomor_ijazah,
-            'active' => $request->input('active', 1),
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now(),
+    public function tambah(Request $request) {
+        $riwayat = \DB::table('riwayat')->insert([
+              'tingkat_pendidikan' => $request->tingkat_pendidikan,
+              'nama_sekolah' => $request->nama_sekolah,
+              'jurusan_program_studi' => $request->jurusan_program_studi,
+              'tahun_masuk' => $request->tahun_masuk,
+              'tahun_lulus' => $request->tahun_lulus,
+              'tempat_belajar' => $request->tahun_lulus,
+              'lokasi' => $request->lokasi,
+              'nomor_ijazah' => $request->nomor_ijazah,      
         ]);
 
-        dd($pendidikan_formal);
-       
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return $riwayat;
     }
 }
