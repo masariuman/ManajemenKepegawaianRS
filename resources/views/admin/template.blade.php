e901c232b97f515fe194fdf35457b86afdc9b92f
masariuman <masariuman@gmail.com>
2020-06-09 10:25:00 +0700

    login done

--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -1,24 +1,26 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        @include('content.meta')
-        <title>Manajemen RSUNTAN</title>
-        @include('content.css')
-    </head>
-    <body>
-        <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
-            @include('content.header')
-            @include('content.ui')
-            <div class="app-main">
-                @include('content.sidebar')
-                <div class="app-main__outer">
-                @include('riwayat_jabatan_teknis.indexContent/content')
-                @include('content.footer')
-                </div>
-                <script src="https://maps.google.com/maps/api/js?sensor=true"></script>
-            </div>
-        </div>
+<head>
+     @include('content.meta')
+     <title>Manajemen RSUNTAN</title>
+     @include('content.css')
+     @stack('css')
+</head>
+<body>
+    <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+     @include('content.header')
+     @include('content.ui')
+     <div class="app-main">
+        @include('content.sidebar')
+        <div class="app-main__outer">
         @yield('content')
-        @stack('css')
-    </body>
+        @include('content.footer')
+        </div>
+        <script src="https://maps.google.com/maps/api/js?sensor=true"></script>
+    </div>
+
+
+    </div>
+    @stack('css')
+</body>
 </html>
