dd9e81640797b79a0bc16d92a0e56bf624218470
masariuman <masariuman@gmail.com>
2020-06-15 13:31:25 +0700

    MODAL Riwayat kepangkatan

--- resources/views/admin/pegawai/show.blade.php
+++ resources/views/admin/pegawai/show.blade.php
@@ -106,7 +106,7 @@
     @include('admin.pegawai.show_partial.modal_partial.riwayat_diklat_fungsional_show')
     @include('admin.pegawai.show_partial.modal_partial.riwayat_diklat_teknis_show')
     @include('admin.pegawai.show_partial.modal_partial.riwayat_diklat_penjenjangan_show')
-    @include('admin.pegawai.show_partial.modal_partial.riwayat_diklat_teknis_show')
+    @include('admin.pegawai.show_partial.modal_partial.riwayat_kepangkatan_show')
     @include('admin.pegawai.show_partial.modal_partial.riwayat_diklat_teknis_show')
     @include('admin.pegawai.show_partial.modal_partial.riwayat_diklat_teknis_show')
     @include('admin.pegawai.show_partial.modal_partial.riwayat_diklat_teknis_show')
