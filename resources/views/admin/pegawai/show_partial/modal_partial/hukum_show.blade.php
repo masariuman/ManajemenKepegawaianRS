1b8062a49ff02dc37dd98307c794c95384d1d1b5
masariuman <masariuman@gmail.com>
2020-06-15 14:20:43 +0700

    modal hukuman

---
+++ resources/views/admin/pegawai/show_partial/modal_partial/hukum_show.blade.php
@@ -0,0 +1,42 @@
+<div class="modal fade bd-example-modal-lg" id="hukum_show" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Detail Data Hukuman Disiplin</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <table class="mb-0 table table-striped">
+                    <tbody>
+                        <tr>
+                            <th scope="row" class="sidetable">KODE HUKUMAN</th>
+                            <td>Mark</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="sidetable">NOMOR SK</th>
+                            <td>Mark</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="sidetable">TANGGAL SK</th>
+                            <td>Mark</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="sidetable">TMT BERLAKU</th>
+                            <td>Mark</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="sidetable">PEJABAT PEMBUAT SK</th>
+                            <td>Mark</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-dismiss="modal"> <i class="fa fa-fw" aria-hidden="true" title="Copy to use times"></i> Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
+
