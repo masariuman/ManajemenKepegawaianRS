6c0ba679d888245f8d7be4c68423fbc088061b9b
masariuman <masariuman@gmail.com>
2020-06-16 15:23:00 +0700

    show diklat teknis

--- resources/views/admin/pegawai/show_partial/riwayat_diklat_teknis.blade.php
+++ resources/views/admin/pegawai/show_partial/riwayat_diklat_teknis.blade.php
@@ -11,14 +11,16 @@
             </tr>
             </thead>
             <tbody>
+                @foreach ($diklat_teknis as $no => $item)
                     <tr>
                         <th class="text-center">1</th>
-                        <td class="text-center">latihan berjoget</td>
-                        <td class="text-center">disana disini disitu</td>
+                        <td class="text-center">{{$item->nama_diklat}}</td>
+                        <td class="text-center">{{$item->lokasi}}</td>
                         <td class="text-center">
                             <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info" title="Lihat Data" data-toggle="modal" data-target="#riwayat_diklat_teknis_show"><i class="fa fa-eye"></i></button>
                         </td>
                     </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
