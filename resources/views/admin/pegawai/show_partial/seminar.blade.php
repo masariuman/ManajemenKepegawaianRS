3a9dfab1bb803f7ea6b63f4e50d3c502db104710
masariuman <masariuman@gmail.com>
2020-06-17 08:11:37 +0700

    seminar

--- resources/views/admin/pegawai/show_partial/seminar.blade.php
+++ resources/views/admin/pegawai/show_partial/seminar.blade.php
@@ -11,14 +11,16 @@
             </tr>
             </thead>
             <tbody>
+                @foreach ($seminar as $no => $item)
                     <tr>
-                        <th class="text-center">1</th>
-                        <td class="text-center">Seminar Skripsi</td>
-                        <td class="text-center">Kampus Untan</td>
+                        <th class="text-center">{{$no+1}}</th>
+                        <td class="text-center">{{$item->nama_kegiatan}}</td>
+                        <td class="text-center">{{$item->lokasi}}</td>
                         <td class="text-center">
                             <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info" title="Lihat Data" data-toggle="modal" data-target="#seminar_show"><i class="fa fa-eye"></i></button>
                         </td>
                     </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
