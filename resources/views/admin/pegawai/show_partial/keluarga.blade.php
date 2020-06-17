f9dff32b32ae8bd635aba7d26705f9209def1202
masariuman <masariuman@gmail.com>
2020-06-17 08:50:25 +0700

    keluarga show done

--- resources/views/admin/pegawai/show_partial/keluarga.blade.php
+++ resources/views/admin/pegawai/show_partial/keluarga.blade.php
@@ -12,15 +12,17 @@
             </tr>
             </thead>
             <tbody>
+                @foreach ($keluarga_kandung as $no => $item)
                     <tr>
-                        <th class="text-center">1</th>
-                        <td class="text-center">si anu</td>
-                        <td class="text-center">Moyang si anu</td>
-                        <td class="text-center">tukang tukangan</td>
+                        <th class="text-center">{{$no+1}}</th>
+                        <td class="text-center">{{$item->nama}}</td>
+                        <td class="text-center">{{$item->hubungan}}</td>
+                        <td class="text-center">{{$item->pekerjaan}}</td>
                         <td class="text-center">
                             <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info" title="Lihat Data" data-toggle="modal" data-target="#keluarga_kandung_show"><i class="fa fa-eye"></i></button>
                         </td>
                     </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
@@ -39,15 +41,17 @@
                 </tr>
                 </thead>
                 <tbody>
+                    @foreach ($keluarga_istri_suami as $no => $item)
                         <tr>
-                            <th class="text-center">1</th>
-                            <td class="text-center">si bapak suami</td>
-                            <td class="text-center">bapak e</td>
-                            <td class="text-center">tukang apa saja</td>
+                            <th class="text-center">{{$no+1}}</th>
+                            <td class="text-center">{{$item->nama}}</td>
+                            <td class="text-center">{{$item->hubungan}}</td>
+                            <td class="text-center">{{$item->pekerjaan}}</td>
                             <td class="text-center">
                                 <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info" title="Lihat Data" data-toggle="modal" data-target="#keluarga_suami_show"><i class="fa fa-eye"></i></button>
                             </td>
                         </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
@@ -58,20 +62,24 @@
                 <thead>
                 <tr>
                     <th class="width40 text-center">No</th>
-                    <th class="text-center">Nama Diklat</th>
-                    <th class="text-center">Lokasi</th>
+                    <th class="text-center">Nama</th>
+                    <th class="text-center">Hubungan</th>
+                    <th class="text-center">Pekerjaan</th>
                     <th class="width100px text-center">Aksi</th>
                 </tr>
                 </thead>
                 <tbody>
+                    @foreach ($keluarga_istri_suami as $no => $item)
                         <tr>
-                            <th class="text-center">1</th>
-                            <td class="text-center">latihan berjoget</td>
-                            <td class="text-center">disana disini disitu</td>
+                            <th class="text-center">{{$no+1}}</th>
+                            <td class="text-center">{{$item->nama}}</td>
+                            <td class="text-center">{{$item->hubungan}}</td>
+                            <td class="text-center">{{$item->pekerjaan}}</td>
                             <td class="text-center">
                                 <button class="mb-2 mr-2 border-0 btn-transition btn btn-outline-info" title="Lihat Data" data-toggle="modal" data-target="#keluarga_istri_show"><i class="fa fa-eye"></i></button>
                             </td>
                         </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
