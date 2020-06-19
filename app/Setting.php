514e57d2698d01d35b3d94d1de5638ec1aa92060
masariuman <masariuman@gmail.com>
2020-06-19 10:53:26 +0700

    setting for skp done

--- app/Setting.php
+++ app/Setting.php
@@ -10,6 +10,8 @@ class Setting extends Model
     protected $table = 'setting';
     protected $fillable = [
         'tahun',
+        'terakhir_isi_skp',
+        'active_skp',
         'created_at',
         'updated_at'
     ];
